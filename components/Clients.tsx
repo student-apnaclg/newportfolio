ad01c0582ddf5ab77c6860d792e9db94aa930285
Nandini <nandiniverma202021@gmail.com>
2024-06-12 09:18:30 +0530

    Changes in client

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { InfiniteMovingCards } from './ui/InfiniteMovingCard';
+import { testimonials } from '@/data';
+import { companies } from '@/data';
+import Image from 'next/image';
 
 const Clients = () => {
   return (
@@ -17,12 +21,12 @@ const Clients = () => {
           <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
             {companies.map(({id,img,name,nameImg})=>(
               <div key={id} className ="flex md:max-w-60 max-w-32 gap-2">
-              <img 
+              <Image
                 src={img}
                 alt={name}
                 className = "md:w-10 w-5"
               />
-              <img 
+              <Image
                 src={nameImg}
                 alt={name}
                 className = "md:w-24 w-20"
